1ed53a19e8736e60c82ad2fb3718904581390fe9
AndreaParrini <andrea.parrini26@gmail.com>
2023-11-15 11:50:01 +0100

    add container in html and function in script.js
    Create file style.css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
-console.log('Hello World');
+let UserName = prompt('Inserisci qui il tuo nome');
+let UserLastName = prompt('Inserisi qui il tuo cognome');
+let UserColor = prompt('Inserisi qui il tuo colore preferito');
+
+let password = document.getElementById('password');
+
+password.innerHTML = UserName + UserLastName + UserColor + "23";
